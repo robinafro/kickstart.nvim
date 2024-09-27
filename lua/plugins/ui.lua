1f8485b508f2bb4e267ffc02fdefee8f18b07f4d
robinafro <oble.ro.2023@skola.ssps.cz>
2024-09-27 15:18:57 +0200

    CTRL+P Binding for finding files

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -188,6 +188,8 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', 'C-p', builtin.find_files, { desc = 'Find Files' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
