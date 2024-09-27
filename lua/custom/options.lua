eb14a4fbffdf71b92b6c0cf68a865cd4122235ae
robinafro <oble.ro.2023@skola.ssps.cz>
2024-09-27 12:33:45 +0200

    Enabled nerd-font, better surround

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -78,4 +78,3 @@ vim.opt.scrolloff = 10
 
 -- No timeout for key sequences
 vim.o.timeoutlen = 0
-
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -476,9 +476,9 @@ return {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup {
-        mappings = {},
-      }
+      -- require('mini.surround').setup {
+      --   mappings = {},
+      -- }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
